34f5783b20004b75ee63e2ea662243d044c22d94
Stephen Wills <stephenrwills94@gmail.com>
2022-07-15 13:51:07 -0500

    finished main method

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+using StaticExercise;
+
+Console.WriteLine("Please enter the temp you wish to convert:");
+var x = double.Parse(Console.ReadLine());
+Console.WriteLine(TempConverter.FahrenheitToCelsius(x));
+Console.WriteLine(TempConverter.CelsiusToFahrenheit(x));
